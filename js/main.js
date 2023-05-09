241dcd72f9f6785e7131e0d95760dbde48da0b4e
Sihlejosias <usihlemakhanya@gmail.com>
2023-05-09 09:54:16 +0200

    Statred a fresh instance

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 var togglebtn = document.getElementsByClassName("toggle-btn")[0];
+var aboutbtn = document.getElementsByClassName("bbtn")[0];
+var projectsbtn = document.getElementsByClassName("pbtn")[0];
 var menu = document.getElementsByClassName("nav")[0];
+var about = document.getElementsByClassName("about-subnav")[0];
+var projects = document.getElementsByClassName("projects-subnav")[0];
 togglebtn.addEventListener("click", function () {
     menu.classList.toggle("active");
+});
+aboutbtn.addEventListener("click", function () {
+    about.classList.toggle("active");
+});
+projectsbtn.addEventListener("click", function () {
+    projects.classList.toggle("active");
 });
